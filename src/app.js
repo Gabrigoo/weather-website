69cc271b55c722219abcb762b8818266d40e3dad
Gabrigoo <gabrigoo@gmail.com>
2021-01-02 12:49:26 +0100

    test commit

---
+++ src/app.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+
+const forecast = require('./utils/forecast');
+const geocode = require('./utils/geocode');
+
+const app = express();
+
+// Define paths for Express config
+const publicDir = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// This tells node to have hbs serve dynamic files from views
+app.set('view engine', 'hbs');
+// This tells node to look in templates views instead of public views
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// This tells node to serve STATIC files from public
+app.use(express.static(publicDir));
+
+// This passes info to hbs
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Gábor Rigó',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Gábor Rigó',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'About Me',
+    name: 'Gábor Rigó',
+    message: 'How can I help you?',
+  });
+});
+
+app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address!'
+    })
+  }
+  geocode(req.query.address, (error, { longitude, latitude, location } = {}) => {
+    if (error) {
+      return res.send({ error });
+    };
+    forecast(longitude, latitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error });
+      };
+      res.send({
+        forecast: forecastData,
+        location: location,
+        address: req.query.address,
+      });
+    });
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: 'About Me',
+    name: 'Gábor Rigó',
+    error: 'Help article not found.',
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: 'About Me',
+    name: 'Gábor Rigó',
+    error: 'Page not found.',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('postman-request');
+
+const forecast = (longitude, latitude, callback) => {
+  const url = 'http://api.weatherstack.com/current?access_key=0c27aad265bf80dfc3e14c44f1c4be85&query=' + latitude + ',' + longitude;
+
+  request({ url, json: true }, (error, { body } = {}) => {
+    if (error) {
+      callback('Unable to connect to weather services!');
+    } else if (body.error) {
+      callback('Unable to find location');
+    } else {
+      const current = body.current;
+      callback(undefined, current.weather_descriptions[0] + ". It is currently " + current.temperature + " degrees out. It feels like " + current.feelslike + " degrees out.");
+    }
+  })
+}
+
+module.exports = forecast;
